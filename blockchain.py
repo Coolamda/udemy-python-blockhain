3aa22160a1ee4b102df7a8d39a836be3fe0b1905
Liam <lw@esyoil.com>
2018-09-02 00:14:45 +0200

    Remove comments

--- blockchain.py
+++ blockchain.py
@@ -2,36 +2,26 @@ blockchain = []
 
 
 def get_last_blockchain_value():
-    """ Returns the last value of the blockchain. """
     if len(blockchain) < 1:
         return None
     return blockchain[-1]
 
 
 def add_transaction(transaction_amount, last_transaction):
-    """ Adds new value to blockchain and last element of current blockchain.
-
-    Arguments:
-        :transaction_amount: The amount to be added.
-        :last_transaction: Last transaction of current blockchain.
-    """
     if last_transaction == None:
         last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
 def get_transaction_amount():
-    """ Asks user for transaction amount. """
     return float(input("Your transaction amount: "))
 
 
 def get_user_choice():
-    """ Asks user to input their choice. """
     return input("Your choice: ")
 
 
 def print_blockchain_elements():
-    """ Prints out each block of blockchain. """
     for block in blockchain:
         print("Block:", block)
     else:
@@ -39,7 +29,6 @@ def print_blockchain_elements():
 
 
 def verify_blockchain():
-    """ Verifies blockchain by comparing first block to previous block in blockchain. """
     is_valid = True
     for block_index in range(len(blockchain)):
         if block_index == 0:
