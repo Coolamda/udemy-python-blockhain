5ba2c04185e5836b2286755c0d943480b741d643
Coolambda <liam.wohlfart0@gmail.com>
2018-09-13 11:39:09 +0200

    Play around with recursion and implement my own version of built in functions

--- playground.py
+++ playground.py
@@ -4,5 +4,36 @@ def unlimited_arguments(*args, **kwargs):
 
 
 unlimited_arguments(1, 2, 3, name="Liam", age=19)
-list = [1, 2, 3]
-print("Stuff: {} {} {}".format(*list))
+format_list = [1, 2, 3]
+print("Stuff: {} {} {}".format(*format_list))
+
+
+def generate_array(n):
+    return list(range(1, n + 1))
+
+
+def square(x):
+    return x * x
+
+
+def my_map(func, list):
+    new_list = []
+    for item in list:
+        new_list.append(func(item))
+    return new_list
+
+
+def even(number):
+    return number % 2 == 0
+
+
+def my_filter(pred, list):
+    new_list = []
+    for item in list:
+        if (pred(item)):
+            new_list.append(item)
+    return new_list
+
+
+def curry_sum(a):
+    return lambda b: a + b
